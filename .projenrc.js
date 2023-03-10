3fb4a53421054e01b66201ae610505470736e055
Fabian <FBosler@gmail.com>
2023-03-10 16:01:14 +0100

    added creds

--- .projenrc.js
+++ .projenrc.js
@@ -1,15 +1,21 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'Fabian',
+  author: 'Fabian Bosler',
   authorAddress: 'FBosler@gmail.com',
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
   name: 'ed-aws-cdk-constructs',
   repositoryUrl: 'https://github.com/FBosler/ed-aws-cdk-constructs.git',
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  autoApproveUpgrades: true,
+  autoApproveOptions: {
+    allowedUsernames: ['FBosler', 'dependabot[bot]'],
+  },
+  deps: ['aws-cdk-lib@^2.1.0', 'constructs@^10.0.5', '@aws-solutions-constructs/core@^2.25.0'],
+  description: 'Package provides opinionated constrcuts for common patterns used in everdrop infrastructure',
   // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  packageName: 'everdrop-aws-cdk-constructs',
 });
+
+project.gitignore.exclude('.env');
+
 project.synth();
